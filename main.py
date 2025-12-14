6f468617599d12bb0a97ca87e0a64dc2ba612e86
Alan Tan <alantanlc@gmail.com>
2025-12-14 15:44:36 +0800

    feat: update imports

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import logging
 
 from src.framework.workflow import Workflow
-from src.action.sample_happy_action import SampleHappyAction
-from src.action.sample_exception_action import SampleExceptionAction
-from src.action.sample_not_executable_action import SampleNotExecutableAction
+from src.action.sample.sample_happy_action import SampleHappyAction
+from src.action.sample.sample_exception_action import SampleExceptionAction
+from src.action.sample.sample_not_executable_action import SampleNotExecutableAction
 
 logging.basicConfig(
     level=logging.INFO,
